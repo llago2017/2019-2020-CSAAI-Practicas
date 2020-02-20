51a6c06ecd9d538677b82119a938fdde9c78bfec
Lorena <l.lago.2017@alumnos.urjc.es>
2020-02-20 10:53:35 +0100

    Casi todos los botones funcionan

--- P2/codigo_java.js
+++ P2/codigo_java.js
@@ -11,43 +11,60 @@ const n3 = document.getElementById("n3")
 const n2 = document.getElementById("n2")
 const n1 = document.getElementById("n1")
 const n0 = document.getElementById("n0")
+const suma = document.getElementById("suma")
+const resta = document.getElementById("resta")
+const multiplica = document.getElementById("multiplica")
+const divide = document.getElementById("divide")
 
 n9.onclick = () => {
     console.log("Click!!");
-    display.innerHTML = "9";
+    display.innerHTML += "9";
 }
 
 n8.onclick = () => {
     console.log("Click!!");
-    display.innerHTML = "8";
+    display.innerHTML += "8";
 }
 n7.onclick = () => {
     console.log("Click!!");
-    display.innerHTML = "7";
+    display.innerHTML += "7";
 }
 n6.onclick = () => {
     console.log("Click!!");
-    display.innerHTML = "6";
+    display.innerHTML += "6";
 }
 n5.onclick = () => {
     console.log("Click!!");
-    display.innerHTML = "5";
+    display.innerHTML += "5";
 }
 n4.onclick = () => {
     console.log("Click!!");
-    display.innerHTML = "4";
+    display.innerHTML += "4";
 }
 n3.onclick = () => {
     console.log("Click!!");
-    display.innerHTML = "3";
+    display.innerHTML += "3";
 }
 n2.onclick = () => {
     console.log("Click!!");
-    display.innerHTML = "2";
+    display.innerHTML += "2";
 }
 n1.onclick = () => {
     console.log("Click!!");
-    display.innerHTML = "1";
+    display.innerHTML += "1";
+}
+n0.onclick = () => {
+    console.log("Click!!");
+    display.innerHTML += "0";
+}
+
+suma.onclick = () => {
+    console.log("Click!!");
+    display.innerHTML += "+";
+}
+n0.onclick = () => {
+    console.log("Click!!");
+    display.innerHTML = "0";
 }
 n0.onclick = () => {
     console.log("Click!!");
