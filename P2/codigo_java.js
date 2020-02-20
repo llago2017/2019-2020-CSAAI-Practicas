52abb8b3bfa2335dc0b9cc8808d72df111fcd4ee
Lorena <l.lago.2017@alumnos.urjc.es>
2020-02-20 10:37:17 +0100

    Escribe en el display los numeros al pulsar

---
+++ P2/codigo_java.js
@@ -0,0 +1,55 @@
+console.log("Ejecutando JS...")
+
+const display = document.getElementById("display")
+const n9 = document.getElementById("n9")
+const n8 = document.getElementById("n8")
+const n7 = document.getElementById("n7")
+const n6 = document.getElementById("n6")
+const n5 = document.getElementById("n5")
+const n4 = document.getElementById("n4")
+const n3 = document.getElementById("n3")
+const n2 = document.getElementById("n2")
+const n1 = document.getElementById("n1")
+const n0 = document.getElementById("n0")
+
+n9.onclick = () => {
+    console.log("Click!!");
+    display.innerHTML = "9";
+}
+
+n8.onclick = () => {
+    console.log("Click!!");
+    display.innerHTML = "8";
+}
+n7.onclick = () => {
+    console.log("Click!!");
+    display.innerHTML = "7";
+}
+n6.onclick = () => {
+    console.log("Click!!");
+    display.innerHTML = "6";
+}
+n5.onclick = () => {
+    console.log("Click!!");
+    display.innerHTML = "5";
+}
+n4.onclick = () => {
+    console.log("Click!!");
+    display.innerHTML = "4";
+}
+n3.onclick = () => {
+    console.log("Click!!");
+    display.innerHTML = "3";
+}
+n2.onclick = () => {
+    console.log("Click!!");
+    display.innerHTML = "2";
+}
+n1.onclick = () => {
+    console.log("Click!!");
+    display.innerHTML = "1";
+}
+n0.onclick = () => {
+    console.log("Click!!");
+    display.innerHTML = "0";
+}
