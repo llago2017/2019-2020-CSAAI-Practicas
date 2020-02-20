023c808737185a0481f892192de3afac54465619
Lorena <l.lago.2017@alumnos.urjc.es>
2020-02-20 09:59:11 +0100

    Pruebas de java

---
+++ P2/L4/ejemplo-1.js
@@ -0,0 +1,6 @@
+console.log("Ejecutando JS...")
+
+const test = document.getElementById("test")
+
+// Paso por consola lo que ponga en el párrafo
+console.log(test.innerHTML);
---
+++ P2/L4/hello.js
@@ -0,0 +1,7 @@
+// Comentario de una sola linea
+
+/* Estos son comentarios
+   de bloque....
+*/
+
+console.log("Hola mundo!!!");
