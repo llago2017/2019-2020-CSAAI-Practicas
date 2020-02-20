940b8e20c7a6fcefa61a22bb9a47fb19fa59fd9c
Lorena Lago Jaime <l.lago.2017@alumnos.urjc.es>
2020-02-20 11:58:11 +0100

    Resta

--- P2/codigo_java.js
+++ P2/codigo_java.js
@@ -19,94 +19,110 @@ const igual = document.getElementById("igual")
 const reset = document.getElementById("reset")
 
 reset.onclick = () => {
-    console.log("Click!!");
-    display.innerHTML = "";
+  console.log("Click!!");
+  display.innerHTML = "";
 }
 
 n9.onclick = () => {
-    console.log("Click!!");
-    display.innerHTML += "9";
+  console.log("Click!!");
+  display.innerHTML += "9";
 }
 
 n8.onclick = () => {
-    console.log("Click!!");
-    display.innerHTML += "8";
+  console.log("Click!!");
+  display.innerHTML += "8";
 }
 n7.onclick = () => {
-    console.log("Click!!");
-    display.innerHTML += "7";
+  console.log("Click!!");
+  display.innerHTML += "7";
 }
 n6.onclick = () => {
-    console.log("Click!!");
-    display.innerHTML += "6";
+  console.log("Click!!");
+  display.innerHTML += "6";
 }
 n5.onclick = () => {
-    console.log("Click!!");
-    display.innerHTML += "5";
+  console.log("Click!!");
+  display.innerHTML += "5";
 }
 n4.onclick = () => {
-    console.log("Click!!");
-    display.innerHTML += "4";
+  console.log("Click!!");
+  display.innerHTML += "4";
 }
 n3.onclick = () => {
-    console.log("Click!!");
-    display.innerHTML += "3";
+  console.log("Click!!");
+  display.innerHTML += "3";
 }
 n2.onclick = () => {
-    console.log("Click!!");
-    display.innerHTML += "2";
+  console.log("Click!!");
+  display.innerHTML += "2";
 }
 n1.onclick = () => {
-    console.log("Click!!");
-    display.innerHTML += "1";
+  console.log("Click!!");
+  display.innerHTML += "1";
 }
 n0.onclick = () => {
-    console.log("Click!!");
-    display.innerHTML += "0";
+  console.log("Click!!");
+  display.innerHTML += "0";
 }
 
 suma.onclick = () => {
-    console.log("Click!!");
-    display.innerHTML += "+";
+  console.log("Click!!");
+  display.innerHTML += "+";
 }
 
 resta.onclick = () => {
-    console.log("Click!!");
-    display.innerHTML += "-";
+  console.log("Click!!");
+  display.innerHTML += "-";
 }
 
 multiplica.onclick = () => {
-    console.log("Click!!");
-    display.innerHTML += "*";
+  console.log("Click!!");
+  display.innerHTML += "*";
 }
 
 divide.onclick = () => {
-    console.log("Click!!");
-    display.innerHTML += "÷";
+  console.log("Click!!");
+  display.innerHTML += "÷";
 }
 
 igual.onclick = () => {
-    console.log("Click!!");
-    operacion = display.innerHTML;
+  console.log("Click!!");
+  operacion = display.innerHTML;
 
-    if (operacion.includes('+')) {
-      opera = operacion.split('+');
-      console.log(opera);
+  // Suma
+  if (operacion.includes('+')) {
+    opera = operacion.split('+');
+    console.log(opera);
 
-      if (opera.length > 2) {
-        resultado = parseInt(opera[0]) + parseInt(opera[1]);
-        console.log("resultado -->" + resultado);
-        for (var i = 0; i < (opera.length - 2); i++) {
-        console.log("Voy a sumar --> " + opera[i + 2] );
+    if (opera.length > 2) {
+      resultado = parseInt(opera[0]) + parseInt(opera[1]);
+      console.log("resultado -->" + resultado);
+      for (var i = 0; i < (opera.length - 2); i++) {
+        console.log("Voy a sumar --> " + opera[i + 2]);
         resultado += parseInt(opera[i + 2]);
       }
     } else {
-        resultado = parseInt(opera[0]) + parseInt(opera[1]);
-      }
-
-      console.log(opera);
-
-      display.innerHTML = resultado;
-      }
-
+      resultado = parseInt(opera[0]) + parseInt(opera[1]);
     }
+    console.log(opera);
+  }
+
+  // Resta
+  if (operacion.includes('-')) {
+    opera = operacion.split('-');
+    console.log(opera);
+
+    if (opera.length > 2) {
+      resultado = parseInt(opera[0]) - parseInt(opera[1]);
+      console.log("resultado -->" + resultado);
+      for (var i = 0; i < (opera.length - 2); i++) {
+        console.log("Voy a sumar --> " + opera[i + 2]);
+        resultado += -(parseInt(opera[i + 2]));
+      }
+    } else {
+      resultado = parseInt(opera[0]) - parseInt(opera[1]);
+    }
+    console.log(opera);
+  }
+  display.innerHTML = resultado;
+}
