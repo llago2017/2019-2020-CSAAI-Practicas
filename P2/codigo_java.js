577559d19ca375f7a1015f59a079651272d3031e
Lorena <l.lago.2017@alumnos.urjc.es>
2020-02-27 10:56:40 +0100

    Animacion de teclado funciona

--- P2/codigo_java.js
+++ P2/codigo_java.js
@@ -4,18 +4,6 @@ const display = document.getElementById("display")
 const formula = document.getElementById("formula")
 const display_resultado = document.getElementById("display_resultado")
 
-// Números
-const n9 = document.getElementById("n9")
-const n8 = document.getElementById("n8")
-const n7 = document.getElementById("n7")
-const n6 = document.getElementById("n6")
-const n5 = document.getElementById("n5")
-const n4 = document.getElementById("n4")
-const n3 = document.getElementById("n3")
-const n2 = document.getElementById("n2")
-const n1 = document.getElementById("n1")
-const n0 = document.getElementById("n0")
-
 // Operaciones
 const suma = document.getElementById("suma")
 const resta = document.getElementById("resta")
@@ -46,6 +34,7 @@ function vueltaA0() {
 
 // Mi array para guardar lo que Pongo
 var array = [];
+var cases = [];
 
 // Creará un array con todos los c digitos que hay en el html
 let digitos = document.getElementsByClassName("cdigito")
@@ -96,149 +85,10 @@ function isNumeric(n) {
 
 function calc() {
 
-  prueba = eval(formula.innerHTML);
-  console.log("Prueba --> " + prueba);
-
-  // Este es mi array
-  console.log(array);
-  // Quiero juntar los numeros que están separados
-  for (var i = -1; i < array.length; i++) {
-    count = i;
-    count += 1;
-    if ((isNumeric(array[i]) || array[i] == ".")) {
-      console.log(array[i] + "Es un numero");
-      while (isNumeric(array[count])) {
-        console.log("Voy a juntar --> " + array[i] + " y " + array[count]);
-        array[i] += array[count]
-        array.splice(count, 1);
-        console.log("mi nuevo array --> " + array);
-      }
-      while (array[count] == ".") {
-        array[i] += array[count]
-        array.splice(count, 1);
-        if (isNumeric(array[count])) {
-          console.log("Me falta un numero");
-          while (isNumeric(array[count])) {
-            console.log("El siguiente --> " + array[count]);
-            array[i] += array[count]
-            array.splice(count, 1);
-            console.log("Mi array count --> " + array[count]);
-          }
-        }
-        console.log("mi nuevo array --> " + array);
-      }
-    }
-  }
-
-  // Numero negativo al principio
-  var sig_menos = this.array.indexOf("-");
-  if (sig_menos == 0) {
-  // Hago que el numero siguiente al del signo sea negativo
-  nuevo = this.array[sig_menos] + this.array[sig_menos + 1];
-  this.array[sig_menos] = nuevo;
-  this.array.splice(sig_menos + 1, 1);
-  console.log("array arreglado --> " + array);
-  }
-
-  // Multiplicacion
-  console.log("array actual --> " + array);
-
-  var sig_mult = this.array.indexOf("*");
-  while (sig_mult > 0) {
-    operacion = (parseFloat(this.array[sig_mult - 1]) * parseFloat(this.array[sig_mult + 1]));
-    // Compruebo si el operando 2 es un signo o un numero
-    if (this.array[sig_mult + 1] != "-") {
-      operacion = (parseFloat(this.array[sig_mult - 1]) * parseFloat(this.array[sig_mult + 1]));
-      this.array[sig_mult] = operacion;
-      this.array.splice(sig_mult - 1, 1);
-      this.array.splice(sig_mult, 1);
-      sig_mult = this.array.indexOf("*");
-      console.log(array);
-
-    } else {
-      console.log("Entro en parte negativa");
-      nuevo = this.array[sig_mult + 1] + this.array[sig_mult + 2];
-      console.log("mi nuevo numero negativo --> " + nuevo);
-      // Ejemplo --> [8,*,-,1]
-      this.array[sig_mult + 1] = nuevo;
-      this.array.splice(sig_mult + 2, 1);
-      // Ejemplo --> [8,*,-1]
-      operacion = (parseFloat(this.array[sig_mult - 1]) * parseFloat(this.array[sig_mult + 1]));
-
-      // Elimino lo que he utilizado y dejo el resultado
-      this.array[sig_mult] = operacion;
-      this.array.splice(sig_mult - 1, 1);
-      this.array.splice(sig_mult, 1);
-      sig_mult = this.array.indexOf("*");
-    }
-  }
+  resultado = eval(formula.innerHTML);
+  console.log("Prueba --> " + resultado);
 
 
-  // Division
-  var sig_div = this.array.indexOf("÷");
-  while (sig_div > 0) {
-    if (this.array[sig_div + 1] != "-") {
-      console.log("Todo es positivo");
-      operacion = (parseFloat(this.array[sig_div - 1]) / parseFloat(this.array[sig_div + 1]));
-
-      this.array[sig_div] = operacion;
-      this.array.splice(sig_div - 1, 1);
-      this.array.splice(sig_div, 1);
-      sig_div = this.array.indexOf("÷");
-      console.log(array);
-    } else {
-      console.log("Hay un numero negativo");
-      nuevo = this.array[sig_div + 1] + this.array[sig_div + 2];
-      console.log("mi nuevo numero -->" + nuevo);
-      this.array[sig_div + 1] = nuevo;
-      this.array.splice(sig_div + 2, 1);
-
-      operacion = (parseFloat(this.array[sig_div - 1]) / parseFloat(this.array[sig_div + 1]));
-
-      this.array[sig_div] = operacion;
-      this.array.splice(sig_div - 1, 1);
-      this.array.splice(sig_div, 1);
-      sig_div = this.array.indexOf("÷");
-    }
-
-  }
-
-  // RESTA
-  var sig_menos = this.array.indexOf("-");
-  console.log("El signo está en la posicion -->" + sig_menos);
-
-  while (sig_menos > 0) {
-    operacion = (parseFloat(this.array[sig_menos - 1]) - parseFloat(this.array[sig_menos + 1]));
-
-    this.array[sig_menos] = operacion;
-    this.array.splice(sig_menos - 1, 1);
-    this.array.splice(sig_menos, 1);
-    sig_menos = this.array.indexOf("-");
-    console.log(array);
-  }
-
-  // SUMA
-  // Cuando no existe el signo me da -1
-  var sig_mas = this.array.indexOf("+");
-  console.log(sig_mas);
-  console.log(array);
-  // En el caso de que exista el operador de suma
-  while (sig_mas > 0) {
-    operacion = (parseFloat(this.array[sig_mas - 1]) + parseFloat(this.array[sig_mas + 1]));
-    console.log(operacion);
-    // Cambio el resultado por la operacion en el array
-    this.array[sig_mas] = operacion;
-    this.array.splice(sig_mas - 1, 1);
-    this.array.splice(sig_mas, 1);
-    // Vuelvo a buscar la posicion de +
-    sig_mas = this.array.indexOf("+");
-    console.log(array);
-  }
-
-  if (operacion != null) {
-    resultado = operacion;
-  }
-  array = [resultado];
   display.id = "display_activo";
   formula.id = "formula_activa";
   display_resultado.innerHTML = resultado;
@@ -251,14 +101,25 @@ igual.onclick = () => {
   calc();
 }
 
+
 function init() {
+  // IDEA: CAMBIAR LA ANIMACION, TENGO QUE CAMBIAR COMO COGE LA ID
+
+ // Creo un array con todos los keyCode de los digitos
+  for (var i = 0; i < 10; i++) {
+    inic = 96;
+    inic += i;
+    cases.push(inic);
+    console.log("array de casos --> " + cases);
+  }
+
   document.onkeydown = function(ev) {
     switch (ev.keyCode) {
 
       case 96: // n0
         formula.innerHTML += "0";
         array.push("0");
-        n0.id = "active";
+        digitos[9].id = "active";
         /*
         Esta parte se activa 20ms más tarde, para volver al estilo
         anterior, donde el botón no estaba pulsado
@@ -271,7 +132,7 @@ function init() {
       case 97: // n1
         formula.innerHTML += "1";
         array.push("1");
-        n1.id = "active";
+        digitos[8].id = "active";
 
         setTimeout(function() {
           active.id = "id";
@@ -281,7 +142,7 @@ function init() {
       case 98: // n2
         formula.innerHTML += "2";
         array.push("2");
-        n2.id = "active";
+        digitos[7].id = "active";
         setTimeout(function() {
           active.id = "n2";
         }, 175);
@@ -290,7 +151,7 @@ function init() {
       case 99: // n3
         formula.innerHTML += "3";
         array.push("3");
-        n3.id = "active";
+        digitos[6].id = "active";
         setTimeout(function() {
           active.id = "n3";
         }, 175);
@@ -299,7 +160,7 @@ function init() {
       case 100: // n4
         formula.innerHTML += "4";
         array.push("4");
-        n4.id = "active";
+        digitos[5].id = "active";
         setTimeout(function() {
           active.id = "n4";
         }, 175);
@@ -308,7 +169,7 @@ function init() {
       case 101: // n5
         formula.innerHTML += "5";
         array.push("5");
-        n5.id = "active";
+        digitos[4].id = "active";
         setTimeout(function() {
           active.id = "n5";
         }, 175);
@@ -317,7 +178,7 @@ function init() {
       case 102: // n6
         formula.innerHTML += "6";
         array.push("6");
-        n6.id = "active";
+        digitos[3].id = "active";
         setTimeout(function() {
           active.id = "n6";
         }, 175);
@@ -326,7 +187,7 @@ function init() {
       case 103: // n7
         formula.innerHTML += "7";
         array.push("7");
-        n7.id = "active";
+        digitos[2].id = "active";
         setTimeout(function() {
           active.id = "n7";
         }, 175);
@@ -335,7 +196,7 @@ function init() {
       case 104: // n8
         formula.innerHTML += "8";
         array.push("8");
-        n8.id = "active";
+        digitos[1].id = "active";
         setTimeout(function() {
           active.id = "n8";
         }, 175);
@@ -344,7 +205,7 @@ function init() {
       case 105: // n9
         formula.innerHTML += "9";
         array.push("9");
-        n9.id = "active";
+        digitos[0].id = "active";
         setTimeout(function() {
           active.id = "n9";
         }, 175);
@@ -388,7 +249,7 @@ function init() {
         break;
 
       case 111: // divide
-        formula.innerHTML += "÷";
+        formula.innerHTML += "/";
         array.push("÷");
         divide.id = "opcion_activa";
         setTimeout(function() {
