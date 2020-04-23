6b45994384817fdf472d968c8deb800219942a23
llago2017 <l.lago.2017@alumnos.urjc.es>
2020-04-23 09:31:37 +0200

    Inicio modificacion de colores

---
+++ P5/filtro-1.js
@@ -0,0 +1,92 @@
+console.log("Ejecutando JS....")
+
+//-- Obtener elementos del DOM
+const canvas = document.getElementById('canvas');
+const img = document.getElementById('imagesrc');
+const ctx = canvas.getContext('2d');
+
+//-- Acceso al deslizador
+const deslizador_R = document.getElementById('deslizador_R');
+const deslizador_G = document.getElementById('deslizador_G');
+const deslizador_B = document.getElementById('deslizador_B');
+
+//-- Valor del deslizador
+const range_R= document.getElementById('range_R');
+const range_G= document.getElementById('range_G');
+const range_B= document.getElementById('range_B');
+
+//-- Variables
+var change = false;
+
+//-- Función de retrollamada de imagen cargada
+//-- La imagen no se carga instantaneamente, sino que
+//-- lleva un tiempo. Sólo podemos acceder a ella una vez
+//-- que esté totalmente cargada
+img.onload = function () {
+
+  //-- Se establece como tamaño del canvas el mismo
+  //-- que el de la imagen original
+  canvas.width = img.width;
+  canvas.height = img.height;
+
+  //-- Situar la imagen original en el canvas
+  //-- No se han hecho manipulaciones todavia
+  ctx.drawImage(img, 0,0);
+
+  console.log("Imagen lista...");
+};
+
+function modify_color() {
+
+    //-- Mostrar el nuevo valor del deslizador
+    range_R.innerHTML = deslizador_R.value;
+    range_G.innerHTML = deslizador_G.value;
+    range_B.innerHTML = deslizador_B.value;
+
+    //-- Situar la imagen original en el canvas
+    //-- No se han hecho manipulaciones todavia
+
+      ctx.drawImage(img, 0,0);
+
+    //-- Obtener la imagen del canvas en pixeles
+    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    //-- Obtener el array con todos los píxeles
+    let data = imgData.data
+
+    //-- Obtener el umbral de rojo del desliador
+    umbral_R = deslizador_R.value
+    umbral_G = deslizador_G.value
+    umbral_B = deslizador_B.value
+
+    //-- Filtrar la imagen según el nuevo umbral
+    for (let i = 0; i < data.length; i+=4) {
+      if (data[i] > umbral_R){
+        data[i] = umbral_R;
+      }
+      if (data[i+1] > umbral_G) {
+        data[i+1] = umbral_G;
+      }
+      if (data[i+2] > umbral_B) {
+        data[i+2] = umbral_B;
+      }
+    }
+    //-- Poner la imagen modificada en el canvas
+    ctx.putImageData(imgData, 0, 0);
+}
+
+function main() {
+
+  deslizador_R.oninput = () => {
+      modify_color();
+  }
+  deslizador_G.oninput = () => {
+    modify_color();
+  }
+  deslizador_B.oninput = () => {
+    modify_color();
+  }
+}
+
+
+console.log("Fin...");
